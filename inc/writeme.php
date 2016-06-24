f3d825654af998295482b117c93f76c62a6957ab
NemesisAR <nemesis.teufel@gmail.com>
2016-06-24 17:36:01 +0200

    Revert ":tada: Added .gitattributes & .gitignore files"
    This reverts commit 51ff7384303c1833193201138cd0681c293a1f45.
    
    # Conflicts:
    #	.gitignore

--- inc/writeme.php
+++ inc/writeme.php
@@ -1,4 +1,4 @@
-<div id="section5" class="container-fluid background-violet cd-section">
+<div id="section5" class="container-fluid background-violet cd-section marginTop-80">
   <div class="col-xs-12 text-center paddingTop-40 paddingBottom-40">
     <h2 class="title-text1">Do you like this?</h2>
     <button type="button" class="btn btn-lg btn-yellow" onClick="location.href='contact.php'">Write me!</button>
